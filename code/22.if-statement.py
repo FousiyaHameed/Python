74b677dfa526b1fa00cc55b8bcc397eac2c1af4d
FousiyaPH <fousiyanizarali@gmail.com>
2024-09-16 15:57:26 +0100

    conditional if statement

---
+++ code/22.if-statement.py
@@ -0,0 +1,29 @@
+#if statement
+a =10
+b = 15
+if a < b:
+    print("Condition was true")
+
+#if true then print 'Condition was true', otherwiswise just ignore it, so here nothing will print
+if b < a:
+    print("Condition was true")
+
+#if else
+if False:
+    print("was true")
+else:
+    print("was False")
+
+
+if True:
+    print("was true")
+else:
+    print("was False")    
+
+#elif
+if 'b'< 'a':
+    print("This is true")
+elif 'c' < 'd':
+    print("second condition is true")
+else:
+    print("no condition is true")
---
+++ code/23.conditional.py
@@ -0,0 +1,20 @@
+name = input("What is your name? ")
+
+if len(name) >= 6:
+    print("Your name is long")
+elif len(name) >= 4:
+    print("Your name is 4 or more chracters")
+else:
+    print("Your name is short")
+
+
+
+
+#pass - The pass statement is used as a placeholder for future code.
+
+name = 'keith'
+if name == 'Kevin':
+    print("Hello Kevin")    
+else:
+    pass # non operational statement
+
