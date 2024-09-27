e1829159b77473042cdf9a5f2c58b19d32015bde
FousiyaPH <fousiyanizarali@gmail.com>
2024-09-27 13:26:27 +0100

    fie moved into folder

--- code/test.py
+++
@@ -1,8 +0,0 @@
-
-# other easy way 
-def is_palindrome(item):
-    print(item)
-    print(item[::-1])
-    return item == item[::-1]
-word = input("Enter a  word to check whether it is palindrome or not: ")
-print(is_palindrome(word))
